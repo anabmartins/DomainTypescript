e3ba088e2066245bcc5d25368302c6d5089dd7b3
ANA BEATRIZ MARTINS <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-06-23 09:22:05 -0300

    novas classes

--- produto.ts
+++ produto.ts
@@ -1,17 +1,44 @@
+//classes de domínio
 class Produto {
-    nome: string;
-    cor: string;
-    tamanho: string;
     id: number;
+    nome: string;
+    // ? = propriedade cor e tamanho pode ser undefined
+    cor?: string;
+    tamanho?: string;
+}
+
+class Estoque {
+    produto: Produto;
     qtd: number;
-    constructor(nome: string, cor: string, tamanho: string, id: number, qtd: number) {
-        this.nome = nome;
-        this.cor = cor;
-        this.tamanho = tamanho;
-        this.id = id;
-        this.qtd = qtd;
+    minQtd: number;
+}
+
+class Venda {
+    produto: Produto;
+    qtd: number;
+    lucro: number;
+    timestamp: Date;
+}
+
+class Inventario {
+    produtos: Produto[];
+
+    constructor(){
+        this.produtos = [];
     }
-    rastreamento() {
-        
+
+    addProduto(produto: Produto) {
+        this.produtos.push(produto);
+    }
+
+    getProdutoByID(id: number): Produto | undefined {
+        return this.produtos.find((produto) => produto.id === id);
+    }
+
+    atualizarProduto(produto: Produto) {
+        const index = this.produtos.findIndex((p) => p.id === produto.id);
+        if (index !== -1) {
+            this.produtos[index] = produto;
+        }
     }
 }
