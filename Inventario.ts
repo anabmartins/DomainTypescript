7fb3c851530531a7c2b5f790a0894903b325186a
ANA BEATRIZ MARTINS <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-06-23 10:11:00 -0300

    new

--- Inventario.ts
+++ Inventario.ts
@@ -1,24 +1,34 @@
-import { Produto } from './Produto'
+import { Produto } from './Produto';
 
 export class Inventario {
     produtos: Produto[];
 
     constructor(){
+        // inicializa com array vazio
         this.produtos = [];
     }
 
+// adicionar um produto ao inventário
     addProduto(produto: Produto) {
         this.produtos.push(produto);
     }
-
+ 
+// obter um produto do inventário com base em seu ID
     getProdutoByID(id: number): Produto | undefined {
         return this.produtos.find((produto) => produto.id === id);
+        // retorna o produto encontrado ou undefined
     }
 
     atualizarProduto(produto: Produto) {
+        // encontrar o índice do produto com base no seu ID
         const index = this.produtos.findIndex((p) => p.id === produto.id);
         if (index !== -1) {
             this.produtos[index] = produto;
         }
+    }
+
+    alertaProduto(produto: Produto):boolean {
+        const estoque = this.getProdutoByID(produto);
+        return estoque != undefined && estoque.qtd < estoque.qtd;
     }
 }
--- Main.ts
+++ Main.ts
@@ -1,1 +1,6 @@
+import { Inventario } from './Inventario';
 import { Produto } from './Produto';
+import { Venda } from './Venda';
+import { Estoque } from './Estoque';
+
+new Produto(); 
